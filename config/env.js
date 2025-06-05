6d59c50e1238335917ca9af70199929693f2a86b
kaif85077 <md.20213010@mnnit.ac.in>
2025-06-05 10:59:57 +0530

    set up arcjet for spamming and bot protection

--- config/env.js
+++ config/env.js
@@ -8,3 +8,5 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const DB_URI = process.env.DB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRATION_IN = process.env.JWT_EXPIRATION_IN;
+export const ARCJET_KEY = process.env.ARCJET_KEY;
+export const ARCJET_ENV = process.env.ARCJET_ENV;
